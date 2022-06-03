3ad86a368451b28712dcc3c1503d4222703fbda8
BM-K <klbm126@khu.ac.kr>
2022-06-03 09:53:28 +0900

    Update examples.sh

--- examples.sh
+++ examples.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
  
-CUDA_VISIBLE_DEVICES=1 python main.py --model colbert --ckpt colbert.pt
-CUDA_VISIBLE_DEVICES=1 python eval.py --model colbert --model_path output/colbert.pt 
+CUDA_VISIBLE_DEVICES=0 python main.py --model colbert --ckpt colbert.pt
+CUDA_VISIBLE_DEVICES=0 python eval.py --model colbert --model_path output/colbert.pt 
 
-CUDA_VISIBLE_DEVICES=1 python main.py --model simir --ckpt simir.pt
-CUDA_VISIBLE_DEVICES=1 python eval.py --model simir --model_path output/simir.pt
+CUDA_VISIBLE_DEVICES=0 python main.py --model simir --ckpt simir.pt
+CUDA_VISIBLE_DEVICES=0 python eval.py --model simir --model_path output/simir.pt
